cf9ab276a89e10d17bbef1d8e8ee43ace14b75fc
Stuart Farmer <stuart@lamden.io>
2020-01-07 10:27:41 -0800

    .5?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rocks',
-    version='0.1.4',
+    version='0.1.5',
     packages=['rocks'],
     url='https://www.github.com/Lamden/rocks',
     license='GPL3',
