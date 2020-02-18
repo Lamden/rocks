19deb91ae4f62805af1c28d83f4fee5ff93597ae
Stuart Farmer <stuart@lamden.io>
2020-02-17 16:42:30 -0800

    setup py for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rocks',
-    version='0.1.5',
+    version='0.1.6',
     packages=['rocks'],
     url='https://www.github.com/Lamden/rocks',
     license='GPL3',
